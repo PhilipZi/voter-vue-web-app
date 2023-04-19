e8833809891ad5b03e68f57a042d693f7ff85a75
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-04-19 13:27:38 +0200

    add global component SubmissionListItem

--- app.js
+++ app.js
@@ -27,15 +27,39 @@ const app = Vue.createApp({
       };
     },
   },
-  methods: {
-    upvote(submissionId) {
-      const submission = this.submissions.find(
-        (submission) => submission.id === submissionId
-      );
+  methods: {},
+});
 
-      submission.votes++;
+app.component("SubmissionListItem", {
+  props: ["submission"],
+  methods: {
+    upvote() {
+      this.submission.votes++;
     },
   },
+  template: `
+  <div class="d-flex">
+    <div class="d-shrink-0">
+      <img src="https://via.placeholder.com/60" alt="" />
+    </div>
+    <div class="flex-grow-1 ms-3">
+      <h5>
+      {{submission.title}}
+      <span
+        class="float-end text-primary"
+        style="cursor: pointer"
+        v-on:click="upvote()"
+        ><i class="fa fa-chevron-up"></i>
+        <strong>{{submission.votes}}</strong></span
+      >
+      </h5>
+    <div v-html="submission.desc"></div>
+      <small class="text-muted"
+      >Eingereicht von: {{submission.author}}</small>
+    </div>
+  </div>
+    
+    `,
 });
 
 const vm = app.mount("#app");
